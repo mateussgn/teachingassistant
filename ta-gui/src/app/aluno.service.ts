52decf96c325f9154c8eab70058211181d30c0b5
Mateus Santana <mateus.santana@liferay.com>
2020-11-07 18:03:10 -0300

    SaaS2 Adcionando Github

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -1,4 +1,4 @@
-import { Injectable }    from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { retry, map } from 'rxjs/operators';
@@ -8,31 +8,31 @@ import { Aluno } from '../../../common/aluno';
 @Injectable()
 export class AlunoService {
 
-  private headers = new HttpHeaders({'Content-Type': 'application/json'});
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   private taURL = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   criar(aluno: Aluno): Observable<Aluno> {
-    return this.http.post<any>(this.taURL + "/aluno", aluno, {headers: this.headers})
-             .pipe( 
-                retry(2),
-                map( res => {if (res.success) {return aluno;} else {return null;}} )
-              ); 
+    return this.http.post<any>(this.taURL + "/aluno", aluno, { headers: this.headers })
+      .pipe(
+        retry(2),
+        map(res => { if (res.success) { return aluno; } else { return null; } })
+      );
   }
 
   atualizar(aluno: Aluno): Observable<Aluno> {
-    return this.http.put<any>(this.taURL + "/aluno",JSON.stringify(aluno), {headers: this.headers})          .pipe( 
-                retry(2),
-                map( res => {if (res.success) {return aluno;} else {return null;}} )
-              ); 
+    return this.http.put<any>(this.taURL + "/aluno", JSON.stringify(aluno), { headers: this.headers }).pipe(
+      retry(2),
+      map(res => { if (res.success) { return aluno; } else { return null; } })
+    );
   }
 
   getAlunos(): Observable<Aluno[]> {
     return this.http.get<Aluno[]>(this.taURL + "/alunos")
-              .pipe(
-                 retry(2)
-               );
+      .pipe(
+        retry(2)
+      );
   }
 
 }
